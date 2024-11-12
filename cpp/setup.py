f0480a7f0aa197c3ba730e6b2842a1ab0d1eb980
anosharahim <anosharahim03@gmail.com>
2024-11-12 01:54:26 +0000

    add sparse l1attention to pip install setup

--- cpp/setup.py
+++ cpp/setup.py
@@ -4,9 +4,10 @@ from torch.utils.cpp_extension import BuildExtension, CppExtension
 setup(
 	name='l1attn_cpp',
 	version = '0.1.0',
-	py_modules=['l1attn_cpp'],
+	py_modules=['l1attn_cpp', 'l1attn_sparse_cpp'],
 	ext_modules=[
-		CppExtension('l1attn_drv_cpp', ['l1attn_drv.cpp']), #extra_compile_args = [""],
+		CppExtension('l1attn_drv_cpp', ['l1attn_drv.cpp']),
+		CppExtension('l1attn_sparse_drv', ['l1attn_sparse_drv.cpp']),
 	],
 	cmdclass={
 		'build_ext': BuildExtension
