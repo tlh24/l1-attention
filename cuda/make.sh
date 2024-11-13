83a3bcbf34fd4b4d0b81f2406be10df667a1cf7c
anosharahim <anosharahim03@gmail.com>
2024-11-13 02:14:33 +0000

    remove unnecessary lines

--- cuda/make.sh
+++ cuda/make.sh
@@ -21,12 +21,8 @@ SITE_PACKAGES_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
 
 # Remove any residual files
 rm -rf "$SITE_PACKAGES_DIR/l1attn_cuda"*
-rm -rf "$SITE_PACKAGES_DIR/l1attn_sparse_cuda"*
-rm -rf "$SITE_PACKAGES_DIR/l1attn_sparse_bidi_cuda"*
 
 pip uninstall l1attn-cuda
-pip uninstall l1attn-sparse-cuda # !! you need to remove both !! 
-pip uninstall l1attn-sparse-bidi-cuda
 
 pip install -e .
 
