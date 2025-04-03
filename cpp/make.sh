220a0fce631cf7eadb7df60345b9db2ace244bd5
Tim Hanson <sideskate@gmail.com>
2025-04-03 13:23:17 -0700

    fixed typo

--- cpp/make.sh
+++ cpp/make.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 rm -rf build dist *.egg-info
 rm -rf $(pip cache dir)
 
@@ -10,20 +10,23 @@ rm -rf "$SITE_PACKAGES_DIR/l1attn_cpp"*
 rm -rf "$SITE_PACKAGES_DIR/l1attn_sparse_cpp"*
 rm -rf "$SITE_PACKAGES_DIR/l1attn_sparse_bidi_cpp"*
 
-echo "no build isolation: be sure to have setuptools, wheel and torch installed>
+echo "no build isolation: be sure to have setuptools, wheel and torch installed"
 pip install --no-build-isolation -e .
 wait
 
-
 echo "Checking installed modules..."
-python -c "
+python <<'EOF'
 import importlib.util
-modules = ['l1attn_cpp', 'l1attn_sparse_cpp', 'l1attn_sparse_bidi_cpp', 
-           'l1attn_drv_cpp', 'l1attn_sparse_drv', 'l1attn_sparse_bidi_drv']
+modules = ['l1attn_cpp', 'l1attn_sparse_cpp',
+			'l1attn_sparse_bidi_cpp',
+			'l1attn_drv_cpp', 'l1attn_sparse_drv',
+			'l1attn_sparse_bidi_drv']
 for module in modules:
-    try:
-        __import__(module)
-        print(f'{module}: Installed')
-    except ImportError:
-        print(f'{module}: Not installed')
-"
+	try:
+		__import__(module)
+		print(f'{module}: Installed')
+	except ImportError:
+		print(f'{module}: Not installed')
+
+EOF
+
