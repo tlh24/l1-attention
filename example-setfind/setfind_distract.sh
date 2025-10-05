9ce2641256e06babe09cc9483fb224a30bd8ec4d
Tim Hanson <sideskate@gmail.com>
2025-10-05 13:05:50 -0700

    need to run a distract sweep

--- example-setfind/setfind_distract.sh
+++ example-setfind/setfind_distract.sh
@@ -2,8 +2,8 @@
 
 # Array of training data sizes
 # data_sizes=(32 48 64 80 96 128 160 192 224 256 320 384 448 512 640 768)
-# data_sizes=(32 64 128 256 512 1024 2048 4096)
-data_sizes=(4096)
+data_sizes=(32 64 128 256 512 1024 2048 4096)
+# data_sizes=(4096)
 
 # Function to run the script with specified parameters
 run_script() {
